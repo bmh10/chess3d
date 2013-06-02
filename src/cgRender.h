feeda0ecd26de0e0719419506a58ae764e6b3473
bmh10 <bmh10@imperial.ac.uk>
2013-06-02 08:50:41 +0100

    Added files

---
+++ src/cgRender.h
@@ -0,0 +1,17 @@
+#ifndef _CG_RENDER_H
+
+#define _CG_RENDER_H
+
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include  <vector>
+
+using namespace std;
+
+#include <fstream>
+#include <iostream>
+
+#endif
